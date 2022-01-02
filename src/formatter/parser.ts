c2a603d47da454a7d36a59ba131f99afd56751e6
Sven Richter <sveri80@gmail.com>
2022-01-02 10:50:44 +0100

    ability to parse instance with name and brackets and empty body

--- src/formatter/parser.ts
+++ src/formatter/parser.ts
@@ -1,165 +1,182 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import * as nearley from 'nearley';
 import { default as myGrammar } from '../grammar';
 import { parseInstance } from './instance.parser';
 import { parseClass } from './class.parser';
 
-import { createToken, Lexer, CstParser, EmbeddedActionsParser  } from "chevrotain";
+import { createToken, Lexer, CstParser, EmbeddedActionsParser } from "chevrotain";
 import { resourceLimits } from 'worker_threads';
 
 
 // const singleComment = createToken({ name: "singleComment", pattern: /\/\// });
-const lCurly = createToken({ name: "LCurly", pattern: /{/ });
-const rCurly = createToken({ name: "RCurly", pattern: /}/ });
-const lSquare = createToken({ name: "LSquare", pattern: /\[/ });
-const rSquare = createToken({ name: "RSquare", pattern: /]/ });
-const comma = createToken({ name: "Comma", pattern: /,/ });
-const colon = createToken({ name: "Colon", pattern: /:/ });
+const LParen = createToken({ name: "LParen", pattern: /\(/ });
+const RParen = createToken({ name: "RParen", pattern: /\)/ });
+const LCurly = createToken({ name: "LCurly", pattern: /{/ });
+const RCurly = createToken({ name: "RCurly", pattern: /}/ });
+const LSquare = createToken({ name: "LSquare", pattern: /\[/ });
+const RSquare = createToken({ name: "RSquare", pattern: /]/ });
+const Comma = createToken({ name: "Comma", pattern: /,/ });
+const Colon = createToken({ name: "Colon", pattern: /:/ });
+const Assignment = createToken({ name: "Assignment", pattern: /=/ });
+const Instance = createToken({ name: "Instance", pattern: /instance/ });
+const PubDef = createToken({ name: "PubDef", pattern: /pub def/ });
+const Pub = createToken({ name: "Pub", pattern: /pub / });
+const Identifier = createToken({ name: "Identifier", pattern: /[a-zA-Z$]\w*/ });
 
-const stringLiteral = createToken({
-  name: "StringLiteral",
-  pattern: /"(:?[^\\"]|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/
-});
 
-const singleComment = createToken({
+const SingleComment = createToken({
   name: "singleComment",
   pattern: /\/\/(:?[^\\"]|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*\n|\r/
   // pattern: /\/\/(:?[^\\"]|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*\n\r/
 });
-const numberLiteral = createToken({
-  name: "NumberLiteral",
-  pattern: /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/
-});
-const newLine = createToken({
+
+const WhiteSpace = createToken({
   name: "WhiteSpace",
-  pattern: /[\n\r]+/,
-  group: Lexer.SKIPPED
-});
-const whiteSpace = createToken({
-  name: "WhiteSpace",
-  pattern: /[ \t]+/,
+  pattern: /\s+/,
   group: Lexer.SKIPPED
 });
 
+
 const allTokens = [
-  whiteSpace,
-  singleComment,
-  numberLiteral,
-  stringLiteral,
-  lCurly,
-  rCurly,
-  lSquare,
-  rSquare,
-  comma,
-  colon,
+  WhiteSpace,
+  SingleComment,
+  Instance, PubDef, Pub,
+  LParen, RParen,
+  LCurly, RCurly,
+  LSquare, RSquare,
+  Comma, Colon,
+  Assignment,
+
+  Identifier,
 ];
 const jsonLexer = new Lexer(allTokens);
 
-// class FlixParser extends CstParser {
-//     constructor() {
-//       super(allTokens);
-//       this.performSelfAnalysis();
-//     }
-//     public flix = this.RULE("file", () => {
-//       this.OR([
-//         { ALT: () => this.SUBRULE(this.singleLineComment) },
-//       ]);
-//     });
-    
-//       private singleLineComment = this.RULE("singleLineComment", () => {
-//         this.CONSUME(singleComment);
-//       });
-// }
 
-class FlixParser extends EmbeddedActionsParser  {
-    constructor() {
-      super(allTokens);
-      this.performSelfAnalysis();
-    }
-    public flix = this.RULE("file", () => {
-      let result = "";
-      this.OR([
-        { ALT: () => result += this.SUBRULE(this.singleLineComment) },
-      ]);
+class FlixParser extends EmbeddedActionsParser {
+  tabSize = 4;
+  indentationLevel = 0;
 
-      return result;
-    });
+  constructor() {
+    super(allTokens);
+    this.performSelfAnalysis();
+  }
+
+  private getIndentation() {
+    return " ".repeat(this.tabSize * this.indentationLevel);
+  }
+
+  public flix = this.RULE("file", () => {
+    let result = "";
+    this.OR([
+      { ALT: () => result += this.SUBRULE(this.singleLineComment) },
+      { ALT: () => result += this.SUBRULE(this.instace) },
+    ]);
     
-      private singleLineComment = this.RULE("singleLineComment", () => {
-        const r = this.CONSUME(singleComment);
-        return r.image;
-      });
-}
+    return result;
+  });
   
-  // reuse the same parser instance.
+  private singleLineComment = this.RULE("singleLineComment", () => {
+    const r = this.CONSUME(SingleComment);
+    return r.image;
+  });
+  
+  private instace = this.RULE("instance", () => {
+    this.CONSUME(Instance);
+    // this.CONSUME(WhiteSpace);
+    let identifier = this.CONSUME(Identifier);
+    let instanceType = this.SUBRULE(this.singleBracketWithType);
+    this.CONSUME(LCurly);
+    this.indentationLevel++;
+    let instanceBody = this.SUBRULE(this.instanceBody);
+    this.indentationLevel--;
+    this.CONSUME(RCurly);
+    return "instance " + identifier.image + instanceType + " {\n" + instanceBody + "\n}";
+  });
+  
+  private instanceBody = this.RULE("instanceBody", () => {
+    // let result = "";    
+    // const pubDef = this.CONSUME(PubDef);
+    
+    // return pubDef.image;
+    let result = "";
+    this.OR([
+      { ALT: () => result += this.SUBRULE(this.method)},
+      // { ALT: () => result += this.SUBRULE(this.instace) },
+    ]);
+    
+    return result;
+  });
+  
+  private method = this.RULE("method", () => {
+    let result = "";    
+    const pubDef = this.CONSUME(PubDef);
+    const methodName = this.CONSUME(Identifier);
+    this.CONSUME(LParen);
+    // let argumentsWithType = this.SUBRULE(this.argumentsWithType);
+    this.CONSUME(RParen);
+
+    // this.OR([
+    //   { ALT: () => result += this.SUBRULE(this.singleLineComment) },
+    //   { ALT: () => result += this.SUBRULE(this.instace) },
+    // ]);
+    
+    return this.getIndentation() + pubDef.image + " " + methodName.image + "(" + ")";
+    // return this.getIndentation() + pubDef.image + " " + methodName.image + "(" + argumentsWithType + ")";
+  });
+  
+  // private argumentsWithType = this.RULE("argumentsWithType", () => {
+  //   let param = this.CONSUME(Identifier);
+  //   this.CONSUME(Colon);
+  //   let paramType = this.CONSUME(Identifier);
+  //   return param.image + ":" + paramType.image;
+  // });
+  
+  private singleBracketWithType = this.RULE("singleBracketType", () => {
+    this.CONSUME(LSquare);
+    let instanceType = this.CONSUME(Identifier);
+    this.CONSUME(RSquare);
+    return "[" + instanceType.image + "]";
+  });
+}
+
 const parser = new FlixParser();
 
 export function parse(s: string, tabSize: number) {
-
+  
   const lexResult = jsonLexer.tokenize(s);
-  // setting a new input will RESET the parser instance's state.
+  
+  if(lexResult.errors !== undefined && lexResult.errors.length > 0) {
+    console.log("lexer errors: " + JSON.stringify(lexResult.errors));
+  }
+  
   parser.input = lexResult.tokens;
-  // any top level rule may be used as an entry point
-  return parser.flix().trim();
-  // const cst = parser.flix();
-
-  // // this would be a TypeScript compilation error because our parser now has a clear API.
-  // // let value = parser.json_OopsTypo()
+  if(parser.errors !== undefined && parser.errors.length > 0) {
+    console.log("parser errors: " + JSON.stringify(parser.errors));
+  }
   
-  // let result = {
-  //   // This is a pure grammar, the value will be undefined until we add embedded actions
-  //   // or enable automatic CST creation.
-  //   cst: cst,
-  //   lexErrors: lexResult.errors,
-  //   parseErrors: parser.errors
-  // };
-  // console.log("parseResult: " + JSON.stringify(result));
+  let parsedResult = parser.flix();
+  console.log("parsedResult: " + JSON.stringify(parsedResult));
   
-  // return result;
+  return parsedResult;
 }
 
 
 
-
-
-// function parseResult(results: any, tabSize: number): string {
-//     let resultString = "";
-//     let indentationLevel = 0;
-
-//     console.log("parseResult: " + JSON.stringify(results));
-//     if (results.type === 'main') {
-
-//         // for (const resultBodyArray of results.body as any) {
-//         // console.log("results_array: " + JSON.stringify(resultBodyArray));
-//         for (const bodyArray of results.body as any) {
-//             // if (Array.isArray(body)) {
-//                 // for (const bodyArray of body as any) {
-//                     if (bodyArray.type === 'comment') {
-//                         resultString += bodyArray.text + "\n";
-//                     } else if ( bodyArray.type === 'multiLineComment') {
-//                         resultString += "/*" + bodyArray.text + "*/\n";
-//                     } else if (bodyArray.type === 'instance') {
-//                         resultString += parseInstance(bodyArray, tabSize, indentationLevel) + "\n\n";
-//                     } else if (bodyArray.type === 'class') {
-//                         resultString += parseClass(bodyArray, tabSize, indentationLevel) + "\n\n";
-//                     }
-//                 // }
-//             // }
-//         }
-//         // }
-//     }
-
-//     return resultString.trim();
-// }
-
-
-
-
-// export function parse(s: string, tabSize: number) {
-    // console.log("toParse: " + JSON.stringify(s));
-    // const parser = new nearley.Parser(nearley.Grammar.fromCompiled(myGrammar));
-    // let parsedText = parser.feed(s).results;
-    
-    // // console.log("parsedText " + JSON.stringify(parsedText));
-
-    // return parseResult(parsedText[0], tabSize);
-// }
+// const StringLiteral = createToken({
+//   name: "StringLiteral",
+//   pattern: /"(:?[^\\"]|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/
+// });
+// const NumberLiteral = createToken({
+//   name: "NumberLiteral",
+//   pattern: /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/
+// });
+// const NewLine = createToken({
+//   name: "WhiteSpace",
+//   pattern: /[\n\r]+/,
+//   group: Lexer.SKIPPED
+// });
+// const whiteSpace = createToken({
+//   name: "WhiteSpace",
+//   pattern: /[ \t]+/,
+//   group: Lexer.SKIPPED
+// });
--- src/test/unit/parser-test.spec.ts
+++ src/test/unit/parser-test.spec.ts
@@ -59,13 +59,21 @@ foobar
     });
 
 
-    it(`instance with pub def`, () => {
+    it(`single instance with pub def`, () => {
         expect(parse(`instance Add[Float32] {
-    pub def add(x: Float32, y: Float32): Float32 = $FLOAT32_ADD$(x, y)
+    pub def add()
 }`, 4)).to.equal(`instance Add[Float32] {
     pub def add(x: Float32, y: Float32): Float32 = $FLOAT32_ADD$(x, y)
 }`);
     });
+
+//     it(`single instance with pub def`, () => {
+//         expect(parse(`instance Add[Float32] {
+//     pub def add(x: Float32, y: Float32): Float32 = $FLOAT32_ADD$(x, y)
+// }`, 4)).to.equal(`instance Add[Float32] {
+//     pub def add(x: Float32, y: Float32): Float32 = $FLOAT32_ADD$(x, y)
+// }`);
+//     });
 
     it(`instance with pub def and comments`, () => {
         expect(parse(`// foo
